7f8a3496d826fb9eeca95e3f880bfc5346cdedcd
rajput-ayush <rajput13092001@gmail.com>
2024-08-07 00:36:56 +0530

    commit

--- api/index.js
+++ api/index.js
@@ -16,5 +16,5 @@ mongoose.connect(process.env.MONGO)
 const app=express();
 
  app.listen(3000,()=>{
-    console.log('server is running on port 3000!!');
+    console.log('server is running on port 3000!!!');
  })
