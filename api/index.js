09cba78200d56ba885b0c2381d753f9c8d770484
rajput-ayush <ankitkr2909@gmail.com>
2024-08-07 20:03:22 +0530

    complete sign up page functionality

--- api/index.js
+++ api/index.js
@@ -29,7 +29,8 @@ app.use(express.json());
  app.use((err,req,res,next)=>{
    const statusCode=err.statusCode|| 500;
    const message=err.message || 'Internal server Error';
-   res.status(statusCode).json({success:false,
+   res.status(statusCode).json({
+      success:false,
       statusCode
       ,message});
  });
