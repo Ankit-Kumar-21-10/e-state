5a71caa32b46db9ca807c73661cc2431a648f695
rajput-ayush <ankitkr2909@gmail.com>
2024-08-08 22:41:24 +0530

    create user update api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { t } from '../controllers/user.controller.js'; 
+import { t, updateUser } from '../controllers/user.controller.js'; 
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router=express.Router();
 
 router.get('/test',t);
+router.post('/update/:id',verifyToken,updateUser);
 
 
 export default router;
