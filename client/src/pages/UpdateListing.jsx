96d692229c63c140b3d3640dd70af1ee5e8fc985
rajput-ayush <ankitkr2909@gmail.com>
2024-08-10 15:23:37 +0530

    complete listing page

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -225,7 +225,9 @@ useEffect(()=>{
                          onChange={handleChange } value={formData.regularPrice}/>
                          <div className='flex flex-col items-center'>
                             <p>Regular Price</p>
-                            <span className='text-xs '>($ / month)</span>
+                            {formData.type === 'rent' && (
+                                 <span className='text-xs'>($ / month)</span>
+                                )}
                          </div>
                         
                     </div>
@@ -236,7 +238,9 @@ useEffect(()=>{
                          onChange={handleChange } value={formData.discountPrice}/>
                          <div className='flex flex-col items-center'>
                             <p>Discounted Price</p>
-                            <span className='text-xs '>($ / month)</span>
+                            {formData.type === 'rent' && (
+                            <span className='text-xs'>($ / month)</span>
+                             )}
                          </div>
                     </div>
                     )
